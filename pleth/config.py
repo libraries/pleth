0ac088718ee0614bfe3ca9353a76a788af47fefa
mohanson <mohanson@outlook.com>
2025-10-16 22:17:12 +0800

    Rewrite objectdict

--- pleth/config.py
+++ pleth/config.py
@@ -1,44 +1,33 @@
+import pleth.objectdict
 import random
 import requests
-import typing
 
 
-class ObjectDict(dict):
-    def __getattr__(self, name: str) -> typing.Any:
-        try:
-            return self[name]
-        except KeyError:
-            raise AttributeError(name)
-
-    def __setattr__(self, name: str, value: typing.Any):
-        self[name] = value
-
-
-develop = ObjectDict({
+develop = pleth.objectdict.ObjectDict({
     'chain_id': 1337,
     'gas_base_fee': 21000,
-    'rpc': ObjectDict({
+    'rpc': {
         'url': 'http://127.0.0.1:8545',
         'qps': 32,
-    })
+    }
 })
 
-mainnet = ObjectDict({
+mainnet = pleth.objectdict.ObjectDict({
     'chain_id': 1,
     'gas_base_fee': 21000,
-    'rpc': ObjectDict({
+    'rpc': {
         'url': 'https://eth.drpc.org',
         'qps': 2,
-    })
+    }
 })
 
-testnet = ObjectDict({
+testnet = pleth.objectdict.ObjectDict({
     'chain_id': 11155111,
     'gas_base_fee': 21000,
-    'rpc': ObjectDict({
+    'rpc': {
         'url': 'https://rpc.sepolia.org',
         'qps': 2,
-    })
+    }
 })
 
 
